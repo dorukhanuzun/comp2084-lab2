fdd427b764e86094cb6bd1a860722a4ef38a55de
Dorukhan Uzun <200427534@student.georgianc.on.ca>
2020-02-03 15:03:42 -0500

    Add project files.

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Products> Product = new List<Products>
+            {
+                new Products {Name = "Laptop"},
+                new Products {Name = "Tablet"},
+                new Products {Name = "Desktop"}
+            };
+            ViewData["Products"] = Product;
+            return View(Product);
+        }
+
+
+        public ActionResult Details(string productName)
+        {
+            string message = HttpUtility.HtmlEncode("Your selected product: " + productName);
+            ViewData["ProductDetails"] = message;
+            return View();
+        }
+    }
+}
---
+++ Lab2/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab2.Models
+{
+    public class Products
+    {
+
+        public Products()
+        {
+            Name = "";
+        }
+
+        public string Name { get; set; }
+
+    }
+}
